dc94f475e1a2208d53434a89b778a24892834197
Galen Scovell <galen.scovell@gmail.com>
2015-03-04 17:19:13 -0800

    Finish Discussion 6

--- Labs/Pre-MT2/Dictionaries.py
+++ Labs/Pre-MT2/Dictionaries.py
@@ -15,3 +15,26 @@ def create_dict_squares(limit):
     squares = {x:x*x for x in range(limit)}
     return squares
 
+
+def replace_all(d, x, y):
+    """Replace all occurences of x as a value (not a key) with y."""
+    for key in d:
+        if d[key] == x:
+            d[key] = y
+
+
+def replace_all_deep(d, x, y):
+    """Given arbitrarily deep dictionary d, replace all occurences of 
+    x as a value (not a key) with y.
+    >>> d = {1: {2: 3, 3: 4}, 2: {4: 4, 5: 3}}
+    >>> replace_all(d, 3, 1)
+    >>> d
+    {1: {2: 1, 3: 4}, 2: {4: 4, 5: 1}}
+    """
+    for key in d:
+        if type(d[key]) is dict:
+            replace_all(d[key], x, y)
+        else:
+            if d[key] == x:
+                d[key] = y
+
